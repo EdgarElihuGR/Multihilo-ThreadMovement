803e6cafcd42ee64091218138b9cd36b95f1ebd2
Edgar Elihu Garcia Ramirez <edgar.elihu.garcia.ramirez@gmail.com>
2019-02-06 23:29:38 -0600

    Un hilo nace, muere, se reproduce

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -13,51 +13,19 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUI extends JFrame implements KeyListener {
+public class GUI extends JFrame{
 
     private MiLabel miLbl;
 
-    public GUI() {
-        super("Threads");
-        this.setBounds(400, 100, 500, 500);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(null);
-        addKeyListener(this);
-
-        miLbl = new MiLabel();
-        miLbl.setText("Thread");
-
-        getContentPane().add(miLbl);
-        new Thread(miLbl).start();
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
-
-            miLbl.setLocation(miLbl.getX(), miLbl.getY() - 5);
-        }
-        if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
-            miLbl.setLocation(miLbl.getX(), miLbl.getY() + 5);
-        }
-        if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
-            miLbl.setLocation(miLbl.getX() - 5, miLbl.getY());
-        }
-        if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
-            miLbl.setLocation(miLbl.getX() + 5, miLbl.getY());
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
+	public GUI() {
+		super();
+		this.setBounds(400, 100, 500, 500);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(null);
+		miLbl = new MiLabel(this);
+		miLbl.setText("");
+		miLbl.setOpaque(true);
+		this.add(miLbl);
+		new Thread(miLbl).start();
+	}
 }
--- src/gui/MiLabel.java
+++ src/gui/MiLabel.java
@@ -3,138 +3,72 @@ package gui;
 import java.awt.Color;
 import java.awt.Font;
 import java.util.Random;
+import javax.swing.JFrame;
 
 import javax.swing.JLabel;
 
 public class MiLabel extends JLabel implements Runnable {
 
-    private int left = 0, right = 465, top = 0, bottom = 420;
-    private int x = right / 2, y = bottom / 2;
-    private int numRan;
-    Random ran = new Random();
+    	private JFrame window;
 
-    public MiLabel() {
-        super();
-        this.setLocation(x, y);
-        this.setVisible(true);
-    }
+	Random ran = new Random();
+	private int num;
 
-    @Override
-    public void run() {
+	public MiLabel(JFrame window) {
+		super();
+		this.window = window;
+		this.setVisible(true);
+		this.setLocation((int)(Math.random() * window.getWidth()), (int)(Math.random() * window.getHeight()));
+		this.setSize(20, 20);
+		this.setOpaque(true);
+		this.setText("");
+		num = ran.nextInt(8) + 1;
+		switch (num) {
+		case 1:
+			this.setBackground(Color.RED);
+			break;
+		case 2:
+			this.setBackground(Color.BLUE);
+			break;
+		case 3:
+			this.setBackground(Color.CYAN);
+			break;
+		case 4:
+			this.setBackground(Color.DARK_GRAY);
+			break;
+		case 5:
+			this.setBackground(Color.MAGENTA);
+			break;
+		case 6:
+			this.setBackground(Color.ORANGE);
+			break;
+		case 7:
+			this.setBackground(Color.YELLOW);
+			break;
+		case 8:
+			this.setBackground(Color.GREEN);
+			break;
+		default:
+			break;
+		}
+		System.out.println("Location: "+this.getLocation().getX()+","+this.getLocation().getY());
+		this.window.add(this);
+		this.window.repaint();
+	}
 
-        try {
-
-            //Thread.sleep(2000);
-            while (true) {
-
-                numRan = ran.nextInt(8) + 1;
-
-                if (numRan == 1) {
-
-                    moverDerecha();
-                } else if (numRan == 2) {
-
-                    moverIzquierda();
-                } else if (numRan == 3) {
-
-                    moverArriba();
-                } else if (numRan == 4) {
-
-                    moverAbajo();
-                } else if (numRan == 5) {
-
-                    moverDerechaArriba();
-                } else if (numRan == 6) {
-
-                    moverDerechaAbajo();
-                } else if (numRan == 7) {
-
-                    moverIzquierdaArriba();
-                } else {
-
-                    moverIzquierdaAbajo();
-                }
-
-            }
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    public void moverDerecha() throws InterruptedException {
-
-        while (x <= 435) {
-            Thread.sleep(7);
-            x += 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverIzquierda() throws InterruptedException {
-
-        while (x >= 0) {
-            Thread.sleep(7);
-            x -= 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverArriba() throws InterruptedException {
-
-        while (y >= 3) {
-            Thread.sleep(7);
-            y -= 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverAbajo() throws InterruptedException {
-
-        while (y <= 420) {
-            Thread.sleep(7);
-            y += 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverDerechaArriba() throws InterruptedException {
-
-        while (x <= 442 && y >= 0) {
-            Thread.sleep(7);
-            x += 1;
-            y -= 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverDerechaAbajo() throws InterruptedException {
-
-        while (x <= 442 && y <= 420) {
-            Thread.sleep(7);
-            x += 1;
-            y += 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverIzquierdaArriba() throws InterruptedException {
-
-        while (x >= 0 && y >= 0) {
-            Thread.sleep(7);
-            x -= 1;
-            y -= 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
-
-    public void moverIzquierdaAbajo() throws InterruptedException {
-
-        while (x >= 0 && y <= 420) {
-            Thread.sleep(7);
-            x -= 1;
-            y += 1;
-            this.setBounds(x, y, 50, 50);
-        }
-    }
+	@Override
+	public void run() {
+		System.out.println("Iniciando...");
+		try {
+			Thread.sleep(500);
+			MiLabel lbl = new MiLabel(window);
+			new Thread(lbl).start();
+			Thread.sleep(500);
+			this.window.remove(this);
+			this.window.repaint();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Terminado...");
+	}
 }
